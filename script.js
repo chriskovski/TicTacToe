29bae5502edfe627ed45760063176188e9660c1d
Chris <christophschmid96@yahoo.de>
2022-07-28 21:26:26 +0200

    I added some Javascript, now you can click on each field once, and you can set Xs and Os alternately, starting with X

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello")
+const fields = Array.from(document.getElementsByClassName('field'));
+/* const playerX = document.getElementsByClassName('fa-x');
+const playerO = document.getElementsByClassName('fa-o'); */
+
+let circleTurn = false;
+
+const gameBoard = (() => {
+
+  fields.forEach(field => {
+    field.addEventListener('click', handleClick, {once: true})
+  })
+
+  function handleClick(e){
+    let cell = e.target;
+    let cellX = e.target.querySelector('.fa-x');
+    let cellO = e.target.querySelector('.fa-o');
+
+    if (!circleTurn){
+      cellX.style.visibility = "visible";
+      cell.style.cursor = "not-allowed";
+      circleTurn = true;
+    }else{
+      cellO.style.visibility = "visible";
+      cell.style.cursor = "not-allowed";
+      circleTurn = false;
+    }
+  }
+})();
